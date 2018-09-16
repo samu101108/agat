79fc3fd6c651b9de5a462b6ea474aebf2549d39e
Samuel Barbosa <samu101108@gmail.com>
2018-09-16 00:55:59 -0300

    Definidos estilos gerais para as páginas e feita pequenas modificações nos formulários.

--- agat/src/app/app.component.ts
+++ agat/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AGAT';
-  subtitle = 'Aplicativo de Geolocalização de Assitência Técnicas';
+  subtitle = 'Aplicativo de Geolocalização de Assitências Técnicas';
 }
